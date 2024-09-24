7bf5c4ff84a1f1519ca690b688976222f43ce1c1
Nam (Nick) N. HUYNH <nam.profittask@gmail.com>
2024-09-24 10:56:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -137,5 +137,20 @@ async def list_models():
         ],
     }
 
+@app.get("/")
+async def home():
+    return {
+        "object": "list",
+        "data": [
+            {
+                "id": "gpt-3.5-turbo-0125",
+                "object": "model",
+                "created": 1692901427,
+                "owned_by": "system",
+            }
+        ],
+    }
+
+
 
 # To run the app, use `uvicorn main:app --reload`
